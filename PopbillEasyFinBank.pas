c2590442eb11b7160c4bd1f829faddfdf9168485
JohnJeong123 <frenchofkiss@gmail.com>
2019-12-23 10:08:41 +0900

    Init

---
+++ PopbillEasyFinBank.pas
@@ -0,0 +1,21 @@
+unit PopbillEasyFinBank;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs;
+
+type
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+
+implementation
+
+{$R *.DFM}
+
+end.
